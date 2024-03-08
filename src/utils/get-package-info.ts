c3d63818f6c62f7b02535dd82a4d66032cb5d969
Steidl, Tim (SE-A/34) <tim.steidl@volkswagen.de>
2024-03-08 14:01:21 +0100

    Changed skript to avoid double publishing

--- src/utils/get-package-info.ts
+++ src/utils/get-package-info.ts
@@ -1,7 +1,7 @@
 export function getPackageInfo() {
   return {
     name: "spring-init",
-    version: "1.4.0",
+    version: "1.4.1",
     description: "Spring Boot Generator",
     main: "index.ts",
     scripts: {
